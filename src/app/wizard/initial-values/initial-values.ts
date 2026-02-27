f47a2bc677e0b393e9dd3e94e7417a881e9dfc8f
Tony Hoffmann <sicphyer@gmx.de>
2026-02-27 23:08:55 +0100

    refactor: wizard initials to signal forms

--- src/app/wizard/initial-values/initial-values.ts
+++ src/app/wizard/initial-values/initial-values.ts
@@ -1,90 +1,109 @@
-import { Component, effect, inject, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, signal } from '@angular/core';
 import { select, Store } from '@ngxs/store';
 import { ButtonModule } from 'primeng/button';
 import { FluidModule } from 'primeng/fluid';
-import { InputTextModule } from 'primeng/inputtext';
 import { RippleModule } from 'primeng/ripple';
 import { NextStepAction, SaveWizardData, SetInitials } from '../state/wizard.actions';
 import { InputNumberModule } from 'primeng/inputnumber';
-import { ReactiveFormsModule, FormBuilder, Validators, FormArray, FormGroup } from '@angular/forms';
 import { FieldsetModule } from 'primeng/fieldset';
 import { WizardState } from '../state/wizard.state';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
-import { WizardInitialsModel } from '../domain/wizard-initials.model';
-import { MemberInitialValuesModel } from '../domain/member-initial-values.model';
-import { CategoryInitialValuesModel } from '../domain/category-initial-values.model';
+import { applyEach, form, FormField, FormRoot, required } from '@angular/forms/signals';
+
+interface MemberInitialEntry {
+  member: string;
+  memberId: string;
+  initialIncome: number;
+}
+
+interface CategoryInitialEntry {
+  category: string;
+  categoryId: string;
+  initialExpenseEst: number;
+}
+
+interface InitialsFormModel {
+  accountBalance: number;
+  memberInitials: MemberInitialEntry[];
+  categoryInitials: CategoryInitialEntry[];
+}
 
 @Component({
   selector: 'tbf-initial-values',
   templateUrl: './initial-values.html',
   styleUrls: ['./initial-values.scss'],
-  imports: [ InputTextModule, InputNumberModule, FluidModule, ButtonModule, RippleModule, ReactiveFormsModule, FieldsetModule, InputGroupModule, InputGroupAddonModule ]
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    InputNumberModule,
+    FluidModule,
+    ButtonModule,
+    RippleModule,
+    FieldsetModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    FormField,
+    FormRoot,
+  ],
 })
-export class InitialValues implements OnInit {
+export class InitialValues {
+  private readonly store = inject(Store);
 
-  private store = inject(Store);
-  private fb = inject(FormBuilder);
+  private readonly stateMembers = select(WizardState.members);
+  private readonly stateCategories = select(WizardState.categories);
 
-  private members = select(WizardState.members);
-  public memberGroups = signal<FormGroup[]>([]);
-  memberChangeEffect = effect(() => {
-    let members = this.members();
+  protected readonly initialsModel = signal<InitialsFormModel>({
+    accountBalance: 0,
+    memberInitials: [],
+    categoryInitials: [],
+  });
 
-    let memberarray = this.initialsForm.get('memberInitials') as FormArray;
-    members.forEach( member => {
-      memberarray.push(
-        this.fb.nonNullable.group({
-          name: [member.name, Validators.required],
-          memberId: [member.tempId, Validators.required],
-          initialIncome: [0, Validators.required]
-        })
-      )
-    })
-    this.memberGroups.set(memberarray.controls as FormGroup[]);
-  })
-
-  private categories = select(WizardState.categories);
-  public categoryGroups = signal<FormGroup[]>([]);
-
-  categoryChangeEffect = effect(() => {
-    let categories = this.categories();
-
-    let categoryArray = this.initialsForm.get('categoryInitials') as FormArray;
-    categories.forEach( category => {
-      categoryArray.push(
-        this.fb.nonNullable.group({
-          name: [category.name, Validators.required],
-          categoryId: [category.tempId, Validators.required],
-          initialExpenseEst: [0, Validators.required]
-        })
-      )
-    })
-    this.categoryGroups.set(categoryArray.controls as FormGroup[]);
-  })
-
-  public initialsForm = this.fb.nonNullable.group({
-    accountBalance: [0, Validators.required],
-    memberInitials: this.fb.nonNullable.array<MemberInitialValuesModel>([]),
-    categoryInitials: this.fb.nonNullable.array<CategoryInitialValuesModel>([])
-  })
-
-  // public memberInitialsArray = signal((this.initialsForm.get('memberInitials') as FormArray).controls as);
-  // public categoryInitialsArray = signal(this.initialsForm.get('categoryInitials') as FormArray);
-
-  public memberInitialsArray = this.initialsForm.get('memberInitials') as FormArray;
-  public categoryExpenseEstimationsArray = this.initialsForm.get('categoryInitials') as FormArray;
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    // Sync arrays from wizard state into the form model whenever state changes
+    effect(() => {
+      const members = this.stateMembers();
+      const categories = this.stateCategories();
+      this.initialsModel.update(m => ({
+        ...m,
+        memberInitials: members.map(mb => ({
+          member: mb.name,
+          memberId: mb.tempId,
+          initialIncome: 0,
+        })),
+        categoryInitials: categories.map(cat => ({
+          category: cat.name,
+          categoryId: cat.tempId,
+          initialExpenseEst: 0,
+        })),
+      }));
+    });
   }
 
-  nextStep() {
-    let value = this.initialsForm.value;
-    this.store.dispatch(new SetInitials({accountBalance: value.accountBalance!, memberInitials: value.memberInitials!, categoryInitials: value.categoryInitials!}))
+  protected readonly initialsForm = form(
+    this.initialsModel,
+    (p) => {
+      required(p.accountBalance, { message: 'Kontostand ist erforderlich' });
+      applyEach(p.memberInitials, (item) => {
+        required(item.initialIncome, { message: 'Einkommen ist erforderlich' });
+      });
+      applyEach(p.categoryInitials, (item) => {
+        required(item.initialExpenseEst, { message: 'Schätzung ist erforderlich' });
+      });
+    },
+    {
+      submission: {
+        action: async () => this.nextStep(),
+      },
+    },
+  );
 
-    this.store.dispatch(new SaveWizardData())
+  protected nextStep(): void {
+    const m = this.initialsModel();
+    this.store.dispatch(new SetInitials({
+      accountBalance: m.accountBalance,
+      memberInitials: m.memberInitials,
+      categoryInitials: m.categoryInitials,
+    }));
+    this.store.dispatch(new SaveWizardData());
   }
-
 }
